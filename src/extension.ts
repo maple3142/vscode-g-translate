8573ed7e18622ccf29e69d4c159b9ac2520b77ec
maple3142 <kirby741852963@gmail.com>
2018-08-28 23:57:27 +0800

    support copy

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,44 @@
 'use strict'
 
 import * as vscode from 'vscode'
+import * as clipboardy from 'clipboardy'
 import translate = require('google-translate-api')
 
 export function activate(ctx: vscode.ExtensionContext) {
 	console.log('Activated!')
 	const disposable = vscode.commands.registerCommand('extension.translate', async () => {
-		const text = await vscode.window.showInputBox({
-			placeHolder: 'The text to translate.'
-		})
+		const editor = vscode.window.activeTextEditor
+		const selectedText = editor ? editor.document.getText(editor.selection) : null
+		const text =
+			selectedText ||
+			(await vscode.window.showInputBox({
+				prompt: 'The text to translate'
+			}))
 		if (!text) {
+			vscode.window.showErrorMessage('No text to translate.')
 			return
 		}
 		const from = await vscode.window.showInputBox({
-			placeHolder: 'Text language.',
+			prompt: 'Source language',
 			value: 'auto'
 		})
 		if (!from) {
+			vscode.window.showErrorMessage('No source language.')
 			return
 		}
 		const to = await vscode.window.showInputBox({
-			placeHolder: 'Translate target.',
+			prompt: 'Target language',
 			value: vscode.env.language
 		})
 		if (!to) {
+			vscode.window.showErrorMessage('No target language.')
 			return
 		}
 		const { text: translatedText } = await translate(text, { from, to })
-		vscode.window.showInformationMessage(translatedText)
+		const userAction = await vscode.window.showInformationMessage(translatedText, 'Copy')
+		if (userAction === 'Copy') {
+			clipboardy.write(translatedText)
+		}
 	})
 
 	ctx.subscriptions.push(disposable)
